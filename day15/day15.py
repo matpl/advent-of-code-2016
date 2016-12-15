5e82af25d2432518cb91a86744417601bd069cd7
Mathieu Plourde <mat.plourde@gmail.com>
2016-12-15 00:24:57 -0500

    day 15 clean up

--- day15/day15.py
+++ day15/day15.py
@@ -2,22 +2,22 @@
 with open('input.txt', 'r') as f:
     lines = [line.split(' ') for line in f]
     
-    stuff = []
+    discs = []
     for line in lines:
-        stuff.append([int(line[3]), int(line[11][0])])
+        discs.append([int(line[3]), int(line[11][0])])
     
     # part 2
-    stuff.append([11, 0])
+    discs.append([11, 0])
     
     i = 0
     while(True):
         valid = True
-        for j in range(len(stuff)):
-            if (((i + j + 1) + stuff[j][1]) % stuff[j][0]) != 0:
+        for j in range(len(discs)):
+            if (((i + j + 1) + discs[j][1]) % discs[j][0]) != 0:
                 valid = False
                 break
     
         if valid:
             print(i)
             break
-        i+=1
+        i += 1
